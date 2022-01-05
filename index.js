683c4733329391de284170d4e2c3a3bc7bf1b95c
Sayyam Shah <77239396+Sayyamshah9@users.noreply.github.com>
2022-01-05 22:23:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config({
-    path:'./.env'
+    path:'.env'
 })
 const userRouter = require('./routes/userRoutes.js')
 const crudRouter = require('./routes/crudRoute.js')
